47e20e1b1a595dd88e186dec817526b2d2368536
nua <enuesaa@gmail.com>
2026-08-01 16:43:32 +0900

    feat: setup clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{ArgAction, Parser};
+
+#[derive(Parser, Debug)]
+#[command(version = "v0.0.1", disable_help_flag = true, disable_version_flag = true)]
+struct Args {
+    #[arg(long, help = "Port", default_value_t = 2999)]
+    port: u16,
+    
+    #[arg(long, action = ArgAction::Help, help = "Print help")]
+    help: bool,
+
+    #[arg(long, action = ArgAction::Version, help = "Print version")]
+    version: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{args:?}");
 }
